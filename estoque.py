3503b051e8605dce64f4b9277a3b491d29c7eb14
beatrizgodoysouza-star <beatrizgodoysouza@gmail.com>
2026-05-29 13:39:09 -0300

    Estrutura inicial do SCES

---
+++ estoque.py
@@ -0,0 +1,18 @@
+dadosDoEstoque = [
+    [909, "Rolamento", 20 ,"A-01-03" ]
+    [907, "Engrenagens", 20 ,"A-01-02" ]
+]
+
+
+input("Qual o nome do produto?")
+input(int("Qual é o ID da peça?"))
+
+
+def AdicionarProduto():
+    print("")
+
+def ListarProdutos():
+    print("")
+
+def BuscarID():
+    print("")
