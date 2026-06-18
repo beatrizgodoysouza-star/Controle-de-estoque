28cfa8554ede5af2f9dfd390cc9c3955b8b254b7
beatrizgodoysouza-star <beatrizgodoysouza@gmail.com>
2026-06-18 14:32:01 -0300

    Criei a estrutura básica do menu

--- estoque.py
+++ estoque.py
@@ -1,18 +1,28 @@
 dadosDoEstoque = [
-    [909, "Rolamento", 20 ,"A-01-03" ]
-    [907, "Engrenagens", 20 ,"A-01-02" ]
-]
+    [0, "Martelo", 23, 34],
+    [1, "Rosca", 21, 89],
+    [2, "Engrenagem", 34, 4]
+] ## ID, Nome, quantidade, localização
+novoID = 2
 
 
-input("Qual o nome do produto?")
-input(int("Qual é o ID da peça?"))
+   
+## Menu
+while True: ## Rodar para sempre
+    print("\nBem vindo ao estoque de peças, escolha uma opção:")
+    print("1 - Cadrastar Produto | 2 - Listar os produtos | 3 - Buscar por ID | 4 - Alterar quantidade | 5 - Travar menu")
+    opcao = input("Escolha uma opção: ")
+    if (opcao == "1"):
+        cadrastar_Produto()
+    elif (opcao == "2"):
+         listar_Estoque()     
+    elif (opcao == "3"):
+         buscar_ID()
+    elif (opcao == "4"):
+        alterar_quantidade()
 
 
-def AdicionarProduto():
-    print("")
 
-def ListarProdutos():
-    print("")
+    
 
-def BuscarID():
-    print("")
+
